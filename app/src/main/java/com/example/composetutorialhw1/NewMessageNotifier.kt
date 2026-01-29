30bca0e0acec695c6486a15a3c814661288daab7
HiskiHuhtanen <hise3322@gmail.com>
2026-01-29 17:12:29 +0200

    HW4

--- app/src/main/java/com/example/composetutorialhw1/NewMessageNotifier.kt
+++ app/src/main/java/com/example/composetutorialhw1/NewMessageNotifier.kt
@@ -1,7 +1,10 @@
 package com.example.composetutorialhw1
 
 import android.Manifest
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.compose.runtime.mutableStateOf
@@ -21,11 +24,20 @@ fun pushNotification(context: Context, author: String, text: String) {
         )
     }
 
+    //https://developer.android.com/develop/ui/views/notifications/navigation
+    val resultIntent = Intent(context, MainActivity::class.java)
+    val resultPendingIntent: PendingIntent? = TaskStackBuilder.create(context).run {
+        addNextIntentWithParentStack(resultIntent)
+            getPendingIntent(0,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+        }
+
     val notification = NotificationCompat.Builder(context, "channel_id")
         .setSmallIcon(R.drawable.ic_launcher_foreground)
         .setContentTitle(author)
         .setContentText(text)
         .setPriority(NotificationCompat.PRIORITY_HIGH)
+        .setContentIntent(resultPendingIntent)
         .build()
 
     NotificationManagerCompat.from(context).notify(1, notification)
